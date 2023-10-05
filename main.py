ef3d01cb8dbeb4e11e3c02616b9fa87fa1e7193c
cevheck <cedric.vh@live.be>
2023-10-05 08:56:51 +0200

    add JAX

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import numpy as np
 
 from python.gradient_descent import gradient_descent, gradient_descent_cache
 from python.gradient_descent_native import gradient_descent_native, gradient_descent_native_cache, PyMatrix
+from python.gradient_descent_JAX import gradient_descent_JAX, gradient_descent_cache_JAX
 from python.utils import plot_gradient_descent, animate_gradient_descent
 
 from timeit import timeit
 
-
-def benchmark_gradient_descent(D):
-    secs = timeit(lambda: gradient_descent(D), number=2) / 2
+def benchmark_gradient_descent(X, D):
+    secs = timeit(lambda: gradient_descent(X, D), number=10) / 2
     print(f"Average time python numpy: {secs}")
 
-
-def benchmark_gradient_descent_native(D):
+def benchmark_gradient_descent_JAX(X, D):
+    secs = timeit(lambda: gradient_descent_JAX(X, D), number=10) / 2
+    print(f"Average time JAX: {secs}")
+    
+def benchmark_gradient_descent_native(X, D):
     N = D.shape[0]
     D_native = PyMatrix(D.tolist(), N, N)
-    secs = timeit(lambda: gradient_descent_native(D_native), number=2) / 2
+    secs = timeit(lambda: gradient_descent_native(D_native), number=10) / 2
     print(f"Average time python native: {secs}")
-
 
 
 ### 2D
@@ -47,21 +49,24 @@ def generate_distance_matrix(points):
 
 
 if __name__ == "__main__":
-
     N = 10
     circle = generate_circle_points(N)
     D = np.array(generate_distance_matrix(circle), dtype=np.float64)
     D_native = PyMatrix(D.tolist(), N, N)
 
-    ### Without visuals
-    p = gradient_descent(D)
-    p = gradient_descent_native(D_native)
+    np.random.seed(42)
+    dim = 2
+    X = np.random.rand(N, dim)  # use same starting values X for np and jax
 
-    
+    ### Without visuals
+    p = gradient_descent_JAX(X, D)
+    p2 = gradient_descent(X, D)
+    p3 = gradient_descent_native(D_native)
+
     ### Benchmarks
-    benchmark_gradient_descent(D)
-    benchmark_gradient_descent_native(D)  
-    
+    benchmark_gradient_descent(X, D)
+    benchmark_gradient_descent_native(X, D)  
+    benchmark_gradient_descent_JAX(X, D)  
 
     ### PLOTTING
     P, L = gradient_descent_cache(D, learning_rate=0.0001, num_iterations=1000)
---
+++ python/gradient_descent_JAX.py
@@ -0,0 +1,78 @@
+import jax.numpy as jnp
+import numpy as snp
+import jax
+
+def loss(X, D):
+    N = X.shape[0]
+    total_loss = 0
+    
+    for i in range(N):
+        for j in range(N):
+            difference = X[i] - X[j]
+            squared_distance = jnp.dot(difference.T, difference)
+            total_loss += (squared_distance - D[i, j]**2)**2
+            
+    return total_loss
+
+
+def compute_gradient(X, D):
+    iterations = jnp.arange(X.shape[0])
+    (X, D), grad = jax.lax.scan(iter1, (X, D), iterations)
+    return grad
+
+def iter1(carry, row1):
+    X, D = carry
+    iterations = jnp.arange(X.shape[0])
+    (X, D, row1), grad = jax.lax.scan(calc_single_grad, (X, D, row1), iterations)
+    grad = jnp.sum(grad, axis=0)    
+    return (X, D), grad
+
+def calc_single_grad(carry, row2):
+    X, D, row1 = carry
+    difference = X[row1] - X[row2]
+    #squared_distance = jnp.dot(difference.T, difference)
+    #squared_distance2 = difference @ difference.T
+    grad = jax.tree_util.tree_map(lambda x: 4*((x[row1]- x[row2])@ jnp.transpose((x[row1]- x[row2])) - D[row1, row2]**2) * (x[row1]- x[row2]), (X))
+
+    #squared_distance = difference @ difference.T
+    #grad = 4 * (squared_distance - D[row1, row2]**2) * difference
+    return (X, D, row1), grad
+
+def gradient_descent_JAX(X, D, learning_rate=0.0001, num_iterations=1000):
+    D = jnp.array(D)
+    X = jnp.array(X)
+    
+    iterations = jnp.arange(num_iterations)
+    (X, learning_rate, D), _ = jax.lax.scan(grad_step, (X, learning_rate, D), iterations)
+    return X
+
+def grad_step(carry, x):
+    X, learning_rate, D = carry
+    
+    grad = compute_gradient(X, D)
+    X -= learning_rate * grad
+    return (X, learning_rate, D), None
+
+
+def gradient_descent_cache_JAX(D, learning_rate=0.001, num_iterations=1000):
+    dim = 2
+    N = D.shape[0]
+    key = jax.random.PRNGKey(0)
+    X = jax.random.uniform(key, shape=(N, dim))
+    D = jnp.array(D)
+    
+    iterations = jnp.arange(num_iterations)
+    (X, learning_rate, D), (positions_over_time, loss_over_time) = jax.lax.scan(grad_step_with_time_evolution, (X, learning_rate, D), iterations)
+
+
+    #positions_over_time.append(X.copy())
+    #loss_over_time.append(loss(X, D))
+
+    return positions_over_time, loss_over_time
+
+def grad_step_with_time_evolution(carry, x):
+    X, learning_rate, D = carry
+    loss_val = loss(X, D)
+    grad = compute_gradient(X, D)
+    X -= learning_rate * grad
+    return (X, learning_rate, D), (X,loss_val)

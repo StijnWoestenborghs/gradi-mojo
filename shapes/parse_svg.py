fa588a05918e18b7a505a32ec0eaaab890db9e78
Stijn Woestenborghs <stijn.woestenborghs@sony.com>
2023-10-08 03:19:06 +0200

    include flame shape

--- shapes/parse_svg.py
+++ shapes/parse_svg.py
@@ -3,7 +3,13 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 
 
-def extract_coords_from_svg(svg_file_path):
+def extract_coords_from_svg(svg_file_path, BASE):
+    assert BASE in ["flame", "modular"]
+    scaling = {
+        "modular": 100,
+        "flame": 250
+    }
+
     # Parse the SVG file
     tree = ET.parse(svg_file_path)
     root = tree.getroot()
@@ -20,33 +26,43 @@ def extract_coords_from_svg(svg_file_path):
                 x, y = cmd.split(',')
                 try:
                     # Scale coordinates
-                    x = round(float(x)/100, 6)
-                    y = round(float(y)/100, 6)
+                    scale_factor = scaling[BASE]
+                    x = round(float(x)/scale_factor, 6)
+                    y = round(float(y)/scale_factor, 6)
                     coordinates.append((x, y))
                 except:
                     pass
 
     return coordinates
 
-svg_file_path = './shapes/modular.svg'
-coords = extract_coords_from_svg(svg_file_path)
-points = np.array(list(set(coords)))
+
+def plot_points(points):
+    # Plotting using matplotlib
+    plt.figure(figsize=(10, 10))
+    plt.scatter(points[:, 0], points[:, 1])
+    plt.gca().invert_yaxis()
+    plt.title("Unique SVG Path Coordinates")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True)
+    plt.show()
 
 
-# Plotting using matplotlib
-plt.figure(figsize=(10, 10))
-plt.scatter(points[:, 0], points[:, 1])
-plt.gca().invert_yaxis()
-plt.title("Unique SVG Path Coordinates")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.grid(True)
-plt.show()
+if __name__ == "__main__":
+
+    # BASE = "modular"
+    BASE = "flame"
+    
+    
+    svg_file_path = f'./shapes/{BASE}.svg'
+    coords = extract_coords_from_svg(svg_file_path, BASE)
+    points = np.array(list(set(coords)))
+
+    plot_points(points)
+
+    # Save to csv
+    np.savetxt(f"./shapes/{BASE}.csv", points, delimiter=",", fmt='%f')
 
 
-# Save to csv
-np.savetxt("./shapes/modular.csv", points, delimiter=",", fmt='%f')
-
-
-# load csv
-print(np.loadtxt("./shapes/modular.csv", delimiter=","))
+    # load csv
+    print(np.loadtxt(f"./shapes/{BASE}.csv", delimiter=","))
